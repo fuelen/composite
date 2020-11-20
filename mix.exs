e6748e07a964dea06d350962efa25b9533936529
Artur Plysyuk <ifuelen@gmail.com>
2020-11-20 17:48:28 +0200

    chore: update deps and bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Composite.MixProject do
   def project do
     [
       app: :composite,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
